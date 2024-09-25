b4b2f7cdfcc220d964923b2f8d8d24b3e7c13380
Yashhh10 <200583043@student.georgianc.on.ca>
2024-09-25 13:06:13 -0400

    chnags in main class

--- src/Main.java
+++ src/Main.java
@@ -243,9 +243,9 @@ public class Main{
     public static void main(String[] args) {
         CarRentalSystem rentalSystem = new CarRentalSystem();
 
-        Car car1 = new Car("C001", "Toyota", "Camry", 60.0); // Different base price per day for each car
-        Car car2 = new Car("C002", "Honda", "Accord", 70.0);
-        Car car3 = new Car("C003", "Mahindra", "Thar", 150.0);
+        Car car1 = new Car("C001", "Toyota", "Eclips", 250.0); // Different base price per day for each car
+        Car car2 = new Car("C002", "Toyota", "Supra", 300.0);
+        Car car3 = new Car("C003", "Porsch", "911 Camera GTS", 450.0);
         rentalSystem.addCar(car1);
         rentalSystem.addCar(car2);
         rentalSystem.addCar(car3);
